4be614500a3511c093dca6d50ecdd8366561e0eb
Luis Confraria <luis.confraria@cumulocity.com>
2016-10-18 16:26:53 +0100

    fix realtime example

--- plugins/deviceEventsRealTime/deviceEventsRealtime.controller.js
+++ plugins/deviceEventsRealTime/deviceEventsRealtime.controller.js
@@ -4,39 +4,40 @@
   angular.module('myapp.deviceEventsRealTime')
     .controller('deviceEventsRealTimeCtrl', DeviceEventsRealTimeController);
 
-  function DeviceEventsRealTimeController($scope, $routeParams, c8yRealtime) {
+  function DeviceEventsRealTimeController($scope, $routeParams, c8yRealtime, c8yAlert) {
     var deviceId = $routeParams.deviceId;
     var eventsChannel = '/events/' + deviceId;
+    var SCOPE_ID = $scope.$id;
 
     function startRealtime() {
-      c8yRealtime.start($scope.$id, eventsChannel);
+      c8yRealtime.start(SCOPE_ID, eventsChannel);
     }
 
     function setUpListeners() {
-      c8yRealtime.addListener($scope.$id, $scope.realtimeChannel, $scope.$id + '-subscribed', subscribeListener);
-      c8yRealtime.addListener($scope.$id, $scope.realtimeChannel, 'CREATE', createListener);
-      c8yRealtime.addListener($scope.$id, $scope.realtimeChannel, 'DELETE', deleteListener);
+      c8yRealtime.addListener(SCOPE_ID, eventsChannel, 'CREATE', onCreateEvent);
+      c8yRealtime.addListener(SCOPE_ID, eventsChannel, 'DELETE', onDeleteEvent);
     }
 
     function stopRealtime() {
-      c8yRealtime.stop($scope.$id, eventsChannel);
+      c8yRealtime.stop(SCOPE_ID, eventsChannel);
     }
 
-    function subscribeListener() {
-      alert('works');
+    function onCreateEvent(action, eventObject) {
+      $scope.events.push(eventObject);
     }
 
-    function createListener() {
-      alert('created');
-    }
+    function onDeleteEvent(action, eventObjectId) {
+      _.remove($scope.events, function (evt) {
+        return evt.id === eventObjectId;
+      });
 
-    function deleteListener() {
-      alert('created');
+      c8yAlert.info('Event with id ' + eventObjectId + ' has been deleted.' );
     }
 
     $scope.events = [];
     $scope.$on('$destroy', stopRealtime);
 
+    setUpListeners();
     startRealtime();
   }
 
